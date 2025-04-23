209daa09f16eadb4a4da076116fac596a810f4d8
keziagomesf <keziagomes.ferre@gmail.com>
2025-04-23 15:00:28 -0300

    feat: create Home page with initial responsiveness and styling adjustments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Image from 'next/image'
+import heroImg from "../../public/assets/hero.png"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +21,29 @@ export default function Home() {
         <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
 
       </Head>
-      <h1>Meu projeto</h1>
+      
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+          className={styles.hero}
+          alt="Logo Tarefas+"
+          src={heroImg} 
+          priority
+          />
+        </div>
+        <h1 className={styles.title}>
+            Sistema feito para você organizar <br />seus estudos e tarefas
+        </h1>
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+          <section className={styles.box}>
+            <span>+90 comentários</span>
+          </section>
+        </div>
+      </main>
+
     </div>
   );
 }
