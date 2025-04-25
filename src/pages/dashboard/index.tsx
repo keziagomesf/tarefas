9e65f5555a40b51893f1f4b917805b9b57192687
keziagomesf <keziagomes.ferre@gmail.com>
2025-04-25 11:19:00 -0300

    feat(tasks): add task listing feature to display all user tasks

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,9 +5,9 @@ import { getSession } from 'next-auth/react'
 import {Textarea} from '@/components/textarea/index'
 import {FiShare2} from 'react-icons/fi'
 import {FaTrash} from 'react-icons/fa'
-import { ChangeEvent, FormEvent, useState } from "react"
+import { ChangeEvent, FormEvent, useState, useEffect } from "react"
 import { db } from "@/services/firebaseConnection"
-import { addDoc, collection } from "firebase/firestore"
+import { addDoc, collection, query, orderBy, where, onSnapshot, snapshotEqual } from "firebase/firestore"
 
 interface HomeProps {
     user: {
@@ -15,9 +15,43 @@ interface HomeProps {
     }
 }
 
+interface TaskProps{
+    id: string;
+    created: Date;
+    public: boolean;
+    tarefa: string;
+    user: string;
+}
+
 export default function Dashboard({user}: HomeProps) {
     const [input, setInput] = useState("")
     const [publicTask, setPublicTask] = useState(false)
+    const [tasks, setTasks] = useState<TaskProps[]>([])
+
+    useEffect(() => {
+        async function loadTarefas(){
+            const tarefasRef = collection(db, "tarefas")
+            const q = query(
+                tarefasRef,
+                orderBy("created", "desc"),
+                where("user", "==", user?.email)
+            )
+            onSnapshot(q, (snapshot) => {
+                 let lista = [] as TaskProps[];
+                 snapshot.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        tarefa: doc.data().tarefa,
+                        created: doc.data().created,
+                        user: doc.data().user,
+                        public: doc.data().public
+                    });
+                 });
+                 setTasks(lista);
+            })
+        }
+        loadTarefas()
+    }, [user?.email])
 
     function handleChangePublic(event: ChangeEvent<HTMLInputElement>) {
         setPublicTask(event.target.checked)
@@ -74,8 +108,10 @@ export default function Dashboard({user}: HomeProps) {
                 <section className={styles.taskContainer}>
                     <h1>Minhas Tarefas</h1>
 
-                    <article className={styles.task}>
-                        <div className={styles.tagContainer}>
+                    {tasks.map((item) =>(
+                        <article key={item.id} className={styles.task}>
+                        {item.public && (
+                            <div className={styles.tagContainer}>
                             <label className={styles.tag}>PUBLICO</label>
                             <button>
                                 <FiShare2
@@ -83,8 +119,9 @@ export default function Dashboard({user}: HomeProps) {
                                 color="#3183ff"/>
                             </button>
                         </div>
+                        )}
                         <div className={styles.taskContent}>
-                            <p>Minha primeira tarefa de exemplo</p>
+                            <p>{item.tarefa}</p>
                             <button className={styles.trashButton}>
                                 <FaTrash
                                 size={24}
@@ -92,6 +129,7 @@ export default function Dashboard({user}: HomeProps) {
                             </button>
                         </div>
                     </article>
+                    ))}
                     
                 </section>
             </main>
